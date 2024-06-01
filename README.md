8509d2a6daf1a9e8a8456e4698e347fee6af9f1c
Stephen F. Booth <me@sbooth.org>
2024-06-01 07:45:52 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# SimplePlayer for iOS
+
+An iOS audio player demonstrating [SFBAudioEngine](https://github.com/sbooth/SFBAudioEngine).
+
+SimplePlayer is written in Swift using SwiftUI and supports playback of a single item selected from a list.
---
+++ SimplePlayer/TrackView.swift
@@ -0,0 +1,25 @@
+//
+// Copyright (c) 2011 - 2024 Stephen F. Booth <me@sbooth.org>
+// Part of https://github.com/sbooth/SimplePlayer-iOS
+// MIT license
+//
+
+import SwiftUI
+
+struct TrackView: View {
+	let track: Track
+
+	var body: some View {
+		VStack(alignment: .leading, spacing: 2) {
+			Text(track.metadata.title ?? track.url.lastPathComponent)
+				.font(.headline)
+				.fontWeight(.bold)
+			Text(track.metadata.artist ?? "")
+				.font(.subheadline)
+		}
+	}
+}
+
+#Preview {
+	TrackView(track: Track(url: URL(string: "Santeria.mp3")!))
+}
