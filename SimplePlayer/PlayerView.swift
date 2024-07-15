9a34b6349ce64f27c8c1d8fcdd95fb70716ef8ef
Stephen Booth <me@sbooth.org>
2024-07-15 09:35:49 -0500

    Add playback times to track display (#1)

--- SimplePlayer/PlayerView.swift
+++ SimplePlayer/PlayerView.swift
@@ -11,6 +11,8 @@ struct PlayerView: View {
 	@ObservedObject	var viewModel: PlayerViewModel
 	@Environment(\.presentationMode) var presentationMode
 	@State var currentPosition: Double = 0
+	@State var currentTime: Double = 0
+	@State var remainingTime: Double = 0
 
 	var body: some View {
 		GeometryReader { geometry in
@@ -23,7 +25,7 @@ struct PlayerView: View {
 
 				VStack(spacing: 8) {
 					Text(viewModel.dataModel.nowPlaying?.metadata.title ?? "title")
-						.font(Font.system(.title).bold())
+						.font(.system(.title).bold())
 					Text(viewModel.dataModel.nowPlaying?.metadata.artist ?? "artist")
 						.font(.system(.headline))
 				}
@@ -83,10 +85,31 @@ struct PlayerView: View {
 						.padding(.horizontal, 20.0)
 						.accentColor(.pink)
 						.disabled(viewModel.dataModel.playbackState == .stopped)
+					HStack {
+						Text("\(currentTime as NSNumber, formatter: NumberFormatter.singleFractionDigitFormatter)")
+							.font(.caption)
+							.padding(.leading, 20.0)
+							.offset(CGSize(width: 0, height: -5))
+						Spacer()
+						Text("\(remainingTime as NSNumber, formatter: NumberFormatter.singleFractionDigitFormatter)")
+							.font(.caption)
+							.padding(.trailing, 20.0)
+							.offset(CGSize(width: 0, height: -5))
+					}
 				}
 			}
 		}
-		.onReceive(viewModel.playbackProgress) { currentPosition = $0 }
+		.onReceive(viewModel.playbackTime) {
+			if let progress = $0.progress {
+				currentPosition = progress
+			}
+			if let current = $0.current {
+				currentTime = current
+				if let remaining = $0.remaining {
+					remainingTime = -remaining
+				}
+			}
+		}
 		.onReceive(viewModel.dataModel.$playbackState) {
 			if $0 == .stopped {
 				self.presentationMode.wrappedValue.dismiss()
@@ -111,6 +134,15 @@ extension Track {
 			return Image(systemName: "s.square")
 		}
 	}
+}
+
+extension NumberFormatter {
+	static var singleFractionDigitFormatter = {
+		let formatter = NumberFormatter()
+		formatter.minimumFractionDigits = 1
+		formatter.maximumFractionDigits = 1
+		return formatter
+	}()
 }
 
 #Preview {
